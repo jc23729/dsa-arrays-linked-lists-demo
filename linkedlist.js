60054cb52754660b4b1675540115a70e1e8f04ae
Juan Cruz <jc23729@gmail.com>
2021-03-15 12:03:26 -0400

    update

---
+++ linkedlist.js
@@ -0,0 +1,15 @@
+// Goals
+// Describe what an “abstract data type” means
+// Compare different types of arrays
+// Define singly and doubly linked lists
+// Compare performance characteristics of arrays and lists
+// Implement linked lists in JavaScript
+// Lists
+// A list is an abstract data type
+
+// It describes a set of requirements, not an exact implementation.
+
+// Keep multiple items
+// Can insert or delete items at any position
+// Can contain duplicates
+// Preserves order of items
